024376c9906bbeec4b9e740f935e0424428d10ac
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-10-17 21:27:51 -0500

    8.4 boton enlazado con el segundo activity (ingresar)

--- app/src/main/java/com/example/jj/proyectosoftware1/MainActivity.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.jj.proyectosoftware1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ingresar = (Button)findViewById(R.id.btningresar);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent ingresar = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(ingresar);
+            }
+        });
 }
 }
