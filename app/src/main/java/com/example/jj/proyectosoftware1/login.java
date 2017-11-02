b41ca2eecaec0b31b91f3907bfc7322fff08f1c7
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-11-02 12:55:58 -0500

    9.9

--- app/src/main/java/com/example/jj/proyectosoftware1/login.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/login.java
@@ -5,20 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 Button login;
+EditText txtusur,txtpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login = (Button)findViewById(R.id.btnlogin);
+        txtusur = (EditText)findViewById(R.id.txtusu);
+        txtpass = (EditText)findViewById(R.id.txtpass);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent login = new Intent(login.this, MainActivity.class);
                 startActivity(login);
             }
+
         });
+
+
     }
+   /* public String enviardatosGET(String usu, String pas){
+
+    }
+*/
+
 }
