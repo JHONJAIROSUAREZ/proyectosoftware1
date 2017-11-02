a4189e61afc3d56d7f9bafaf435822affaf137a2
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-11-02 13:03:42 -0500

    10

---
+++ app/src/main/java/com/example/jj/proyectosoftware1/Categorias/Granos.java
@@ -0,0 +1,15 @@
+package com.example.jj.proyectosoftware1.Categorias;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jj.proyectosoftware1.R;
+
+public class Granos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_granos);
+    }
+}
