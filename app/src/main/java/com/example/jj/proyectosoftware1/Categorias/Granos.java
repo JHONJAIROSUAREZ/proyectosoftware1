afb8ca3789756400da2c9186317a258f98d8ca63
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-11-04 14:59:16 -0500

    10.1

--- app/src/main/java/com/example/jj/proyectosoftware1/Categorias/Granos.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/Categorias/Granos.java
@@ -2,14 +2,23 @@ package com.example.jj.proyectosoftware1.Categorias;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.jj.proyectosoftware1.R;
 
 public class Granos extends AppCompatActivity {
-
+Button lent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_granos);
+        lent = (Button)findViewById(R.id.btnlenteja);
+        lent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jj/proyectosoftware1/Main2Activity.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/Main2Activity.java
@@ -1,15 +1,30 @@
 package com.example.jj.proyectosoftware1;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.jj.proyectosoftware1.Categorias.Granos;
 
 public class Main2Activity extends AppCompatActivity {
-
+Button btngranos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        btngranos = (Button)findViewById(R.id.btngranos);
+        btngranos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this,Granos.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
