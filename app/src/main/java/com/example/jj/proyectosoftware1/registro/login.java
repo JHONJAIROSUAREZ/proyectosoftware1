297106f1dc1bebe9ad6def2caa7dacb997e3e9ed
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-11-07 22:44:45 -0500

    10.8 Regitro funcionando y registro

--- app/src/main/java/com/example/jj/proyectosoftware1/registro/login.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/registro/login.java
@@ -1,14 +1,21 @@
 package com.example.jj.proyectosoftware1.registro;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.jj.proyectosoftware1.MainActivity;
 import com.example.jj.proyectosoftware1.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class login extends AppCompatActivity {
 Button login,btnregister;
@@ -23,8 +30,45 @@ EditText txtusur,txtpass;
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent login = new Intent(login.this, MainActivity.class);
-                startActivity(login);
+                final  String codigo_P = txtusur.getText().toString();
+                final  String CONTRASENA = txtpass.getText().toString();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success){
+                                String nombre_P = jsonResponse.getString("nombre_P");
+                                String apellido = jsonResponse.getString("apellido");
+                                String telefono = jsonResponse.getString("telefono");
+                                String direccion = jsonResponse.getString("direccion");
+                                String cedula = jsonResponse.getString("cedula");
+
+                                Intent intent = new Intent(login.this, MainActivity.class);
+                                intent.putExtra("nombre_P",nombre_P);
+                                intent.putExtra("apellido",apellido);
+                                intent.putExtra("telefono",telefono);
+                                intent.putExtra("direccion",direccion);
+                                intent.putExtra("cedula",cedula);
+                                intent.putExtra("codigo_P",codigo_P);
+                                intent.putExtra("CONTASENA",CONTRASENA);
+
+                                login.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(login.this);
+                                builder.setMessage("ERROR EN EL LOGIN")
+                                        .setNegativeButton("RETRY",null)
+                                        .create().show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(codigo_P,CONTRASENA, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(login.this);
+                queue.add(loginRequest);
             }
 
         });
---
+++ app/src/main/java/com/example/jj/proyectosoftware1/registro/usuario.java
@@ -0,0 +1,35 @@
+package com.example.jj.proyectosoftware1.registro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.jj.proyectosoftware1.R;
+
+public class usuario extends AppCompatActivity {
+TextView TextViewnombre,TextViewapellido,TextViewtelefono,TextViewdireccion,TextViewcedula;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+            TextViewnombre = findViewById(R.id.tvnombre);
+        TextViewapellido = findViewById(R.id.tvapellido);
+        TextViewtelefono = findViewById(R.id.tvtelefono);
+        TextViewdireccion = findViewById(R.id.tvdireccion);
+        TextViewcedula = findViewById(R.id.tvcedula);
+
+        Intent intent = getIntent();
+        String nombre_P =intent.getStringExtra("nombre_P");
+        String apellido = intent.getStringExtra("apellido");
+        String  telefono= intent.getStringExtra("telefono");
+        String direccion = intent.getStringExtra("direccion");
+        String  cedula = intent.getStringExtra("cedula");
+
+        TextViewnombre.setText(nombre_P);
+        TextViewapellido.setText(apellido);
+        TextViewtelefono.setText(telefono);
+        TextViewdireccion.setText(direccion);
+        TextViewcedula.setText(cedula);
+    }
+}
