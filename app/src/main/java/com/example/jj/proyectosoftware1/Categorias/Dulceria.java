df8f3821f84f981d7f458fc6ad2c55699044b7f4
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-11-29 13:42:22 -0500

    11.2 cardview agregados

--- app/src/main/java/com/example/jj/proyectosoftware1/Categorias/Dulceria.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/Categorias/Dulceria.java
@@ -1,7 +1,11 @@
 package com.example.jj.proyectosoftware1.Categorias;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.jj.proyectosoftware1.R;
 
@@ -11,5 +15,38 @@ public class Dulceria extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dulceria);
+
+        ListView mListView = (ListView) findViewById (R.id.clientList);
+
+        mListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                //Cliente client = recipeList.elementAt(i);
+                //client.id
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+// 1
+        //final ArrayList <CLient> recipeList = Recipe.getRecipesFromFile ( " recipes.json " , this );
+// 2
+        String [] listItems = new String [3];
+// 3
+        for ( int i = 0 ; i < 3; i++) {
+            //Receta receta = recetaLista.get (i);
+            listItems [i] = ("prueba" + i);
+        }
+// 4
+        ArrayAdapter adapter = new ArrayAdapter ( this , android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter (adapter);
+
+
     }
 }
--- app/src/main/java/com/example/jj/proyectosoftware1/MainActivity.java
+++ app/src/main/java/com/example/jj/proyectosoftware1/MainActivity.java
@@ -12,66 +12,66 @@ import android.widget.Button;
 import com.example.jj.proyectosoftware1.registro.registerclients;
 import com.example.jj.proyectosoftware1.registro.usuario;
 
-public class MainActivity extends AppCompatActivity {
-Button ingresar,modificar,clientes,btnactivos;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        ingresar = findViewById(R.id.btningresar);
-        ingresar.setOnClickListener(new View.OnClickListener() {
+        public class MainActivity extends AppCompatActivity {
+            Button ingresar,modificar,clientes,btnactivos;
             @Override
-            public void onClick(View view) {
-               Intent ingresar = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(ingresar);
-            }
-        });
-        clientes = findViewById(R.id.btncliente);
-        clientes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent clientes = new Intent(MainActivity.this, registerclients.class);
-                startActivity(clientes);
-            }
-        });
-        modificar = findViewById(R.id.btnmodificar);
-        modificar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent modificar = new Intent(MainActivity.this, ModificarP.class);
-                startActivity(modificar);
-            }
-        });
-        btnactivos = findViewById(R.id.btnactivo);
-        btnactivos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent activo = new Intent(MainActivity.this, usuario.class);
-                startActivity(activo);
-            }
-        });
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                ingresar = findViewById(R.id.btningresar);
+                ingresar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent ingresar = new Intent(MainActivity.this, Main2Activity.class);
+                        startActivity(ingresar);
+                    }
+                });
+                clientes = findViewById(R.id.btncliente);
+                clientes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent clientes = new Intent(MainActivity.this, registerclients.class);
+                        startActivity(clientes);
+                    }
+                });
+                modificar = findViewById(R.id.btnmodificar);
+                modificar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent modificar = new Intent(MainActivity.this, ModificarP.class);
+                        startActivity(modificar);
+                    }
+                });
+                btnactivos = findViewById(R.id.btnactivo);
+                btnactivos.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent activo = new Intent(MainActivity.this, usuario.class);
+                        startActivity(activo);
+                    }
+                });
 
-}
+            }
 
-    @Override
-    public void onBackPressed() {
-        final AlertDialog.Builder mybuild = new AlertDialog.Builder(this);
-        mybuild.setMessage("Seguro que quieres salir?");
-        mybuild.setTitle("Salir");
-        mybuild.setPositiveButton("si", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
-            }
-        });
-        mybuild.setNegativeButton("no", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-        AlertDialog dialog = mybuild.create();
-        dialog.show();
+            public void onBackPressed() {
+                final AlertDialog.Builder mybuild = new AlertDialog.Builder(this);
+                mybuild.setMessage("Seguro que quieres salir?");
+                mybuild.setTitle("Salir");
+                mybuild.setPositiveButton("si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                mybuild.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog dialog = mybuild.create();
+                dialog.show();
     }
 }
